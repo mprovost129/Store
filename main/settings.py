d280e7a40bbf8b760c3caf2c02218c87afec27cd
mprovost129 <mprovost129@gmail.com>
2025-08-05 14:02:17 -0400

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -148,10 +148,3 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-# SMTP configuration
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mprovost129@gmail.com'
-EMAIL_HOST_PASSWORD = 'uckrbzdzkfveevxe'
-EMAIL_USE_TLS = True
